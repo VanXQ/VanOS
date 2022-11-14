b5ff9a7017c2fbf093894614f22fbb42a1a527db
Van_XQ <20301174@bjtu.edu.cn>
2022-11-14 19:26:58 +0800

    问题1实现了自己的程序  20301174万兴全

---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/03loop_draw_c-49f365b59e8cc677.d
@@ -0,0 +1,5 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/03loop_draw_c-49f365b59e8cc677: src/bin/03loop_draw_c.rs
+
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/03loop_draw_c-49f365b59e8cc677.d: src/bin/03loop_draw_c.rs
+
+src/bin/03loop_draw_c.rs:
---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/03my_project-bd97669f5acb7800.d
@@ -0,0 +1,5 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/03my_project-bd97669f5acb7800: src/bin/03my_project.rs
+
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/03my_project-bd97669f5acb7800.d: src/bin/03my_project.rs
+
+src/bin/03my_project.rs:
