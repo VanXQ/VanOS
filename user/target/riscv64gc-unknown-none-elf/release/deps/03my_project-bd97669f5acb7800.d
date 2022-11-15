56f64b5c25cc83cc0c911ddbdcea6427f00cb673
Van_XQ <20301174@bjtu.edu.cn>
2022-11-15 11:20:07 +0800

    1.3 20301174-万兴全

--- user/target/riscv64gc-unknown-none-elf/release/deps/03my_project-bd97669f5acb7800.d
+++
@@ -1,5 +0,0 @@
-/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/03my_project-bd97669f5acb7800: src/bin/03my_project.rs
-
-/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/03my_project-bd97669f5acb7800.d: src/bin/03my_project.rs
-
-src/bin/03my_project.rs:
