c17857ff8e42f7f080b221a56653cfc38124fc21
Van_XQ <20301174@bjtu.edu.cn>
2022-12-12 22:00:24 +0800

    课后题 20301174 万兴全

---
+++ user/src/bin/04myproject.rs
@@ -0,0 +1,33 @@
+//自己实现了一个求100以内素数的应用程序
+//user/src/bin/myproject.rs
+#![no_std]
+#![no_main]
+
+// use core::arch::asm;
+
+#[macro_use]
+extern crate user_lib;
+
+#[no_mangle]
+fn main() -> i32 {
+    println!("I will find prime numbers from 1 to 100:");
+    const N: i32 = 100;
+    let mut count = 0;
+    for i in 2..=N {
+        let mut temp = true;
+        for j in 2..i / 2 + 1 {
+            if i % j == 0 {
+                temp = false;
+                break;
+            }
+        }
+        if temp {
+            count += 1;
+            print!("{} ", i);
+            if count % 5 == 0 {
+                println!("");
+            }
+        }
+    }
+    0
+}
---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/04myproject-61f9a80acdd4732d.d
@@ -0,0 +1,5 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/04myproject-61f9a80acdd4732d: src/bin/04myproject.rs
+
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/04myproject-61f9a80acdd4732d.d: src/bin/04myproject.rs
+
+src/bin/04myproject.rs:
